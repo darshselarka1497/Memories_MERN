e51076aba422e6064a38db62ba354b5e0ac73664
dselarka <dselarka@iu.edu>
2021-01-15 14:01:51 +0530

    connected mongodb to application

--- server/index.js
+++ server/index.js
@@ -2,3 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({limit: "30mb", extended: "true"}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: "true"}));
+
+app.cors(cors());
+
+const CONNECTION_URL = 'mongodb+srv://darshselarka:helloworld@cluster0.8cms8.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+//heroku will automatically select from environment port
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .catch(() => )
+
