0ae971bc9e67554040bd98f71d8fbab42cc743de
dselarka <dselarka@iu.edu>
2021-01-15 14:12:24 +0530

    mongodb tweak

--- server/index.js
+++ server/index.js
@@ -18,5 +18,8 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
-    .catch(() => )
+    .catch((error) => console.log(error.message) );
+
+//to make sure to not get any warnings in the console
+mongoose.set('useFindAndModify', false);
 
