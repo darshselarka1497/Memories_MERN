ab90366d287ad87b42814fd8a4cd6391e9395dcf
dselarka <dselarka@iu.edu>
2021-01-15 18:27:33 +0530

    Working on the client (frontend)

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
+import memories from './images/memories.png'
+
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+import Post from './components/Posts/Post/Post';
 
 const App = () => {
     return(
-        <h1>App</h1>
+        <Container maxWidth='lg'>
+            <AppBar position='static' color='inherit'>
+                <Typography variant='h2' align='center'> Memories 
+                <img src={memories} alt="memories" height="50" width="50"/>
+                </Typography>
+            </AppBar>
+            <Grow in>
+                <Container>
+                    <Grid container justify='space-between' alignItems='stretch' spacing='3'>
+                        <Grid item xs={12} sm={7}>
+                            <Posts>
+
+                            </Posts>
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form>
+
+                            </Form>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+        </Container>
     );
 }
 
