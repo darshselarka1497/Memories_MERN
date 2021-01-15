85e92d980e40b63c0b5acac7f4f840f01756c6a9
dselarka <dselarka@iu.edu>
2021-01-15 14:55:03 +0530

    fix db connection

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: "true"}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: "true"}));
 
-app.cors(cors());
+app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://darshselarka:helloworld@cluster0.8cms8.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
@@ -17,7 +17,7 @@ const CONNECTION_URL = 'mongodb+srv://darshselarka:helloworld@cluster0.8cms8.mon
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.log(error.message) );
 
 //to make sure to not get any warnings in the console
